ead5f2f13f557132e11aa1d1ce3918455dba5d14
lenny6033 <lennardtom@hotmail.com>
2021-05-04 15:16:05 +0200

    test

--- MyBot.py
+++ MyBot.py
@@ -17,9 +17,9 @@ import logging
 
 # GAME START
 # Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
-game = hlt.Game("Settler")
+game = hlt.Game("salty triangles")
 # Then we print our start message to the logs
-logging.info("Starting my Settler bot!")
+logging.info("Starting my bot!")
 
 while True:
     # TURN START
